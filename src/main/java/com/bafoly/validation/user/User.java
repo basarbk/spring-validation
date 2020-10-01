c2d6c0a2bb52bee7f6dfcd98ce0a1f1c1d31c9d6
Basar Buyukkahraman <basarbk@yahoo.com>
2020-10-01 23:25:44 +0300

    multiple constraint

--- src/main/java/com/bafoly/validation/user/User.java
+++ src/main/java/com/bafoly/validation/user/User.java
@@ -4,7 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -17,9 +19,11 @@ public class User {
   private long id;
 
   @NotNull
+  @Size(min = 3, max=10)
   private String username;
 
   @NotNull
+  @Email
   private String email;
 
   @NotNull
