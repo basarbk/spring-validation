fee53ef2ea307690706f4c015e0ca4f9219e5503
Basar Buyukkahraman <basarbk@yahoo.com>
2020-10-01 23:13:51 +0300

    post user impl

--- src/main/java/com/bafoly/validation/user/UserController.java
+++ src/main/java/com/bafoly/validation/user/UserController.java
@@ -1,5 +1,19 @@
 package com.bafoly.validation.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+  @Autowired
+  UserService userService;
+
+  @PostMapping("/users")
+  public User createUser(@RequestBody User user){
+    return this.userService.save(user);
+  }
   
 }
--- src/main/java/com/bafoly/validation/user/UserService.java
+++ src/main/java/com/bafoly/validation/user/UserService.java
@@ -1,5 +1,16 @@
 package com.bafoly.validation.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	public User save(User user) {
+		return this.userRepository.save(user);
+	}
   
 }
